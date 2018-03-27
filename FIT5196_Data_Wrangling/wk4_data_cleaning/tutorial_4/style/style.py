7681e15753b78cb96ce109f5629e2bb632604744
zby0902 <zbyroger@gmail.com>
2018-03-27 11:10:12 +1100

    add materials for week4

---
+++ FIT5196_Data_Wrangling/wk4_data_cleaning/tutorial_4/style/style.py
@@ -0,0 +1,7 @@
+from IPython.core.display import HTML
+css = open('style/style-table.css').read() + open('style/style-notebook.css').read()
+HTML('<style>{}</style>'.format(css))
+<div style=" color:black; text-shadow: 1px 1px brown; font-size:2em;  background:url(style/images/Lucerne3.jpg)">,
+    <h1 align="center">Scientific Python
+    <img src="style/images/kundalini_pythons_gold_outline.png" style="height:360px; align:center; " ></h1>
+    </div>
