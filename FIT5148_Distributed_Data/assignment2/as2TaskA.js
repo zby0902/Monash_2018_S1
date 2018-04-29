66b84fbc60377241274203ac1d7130db890fb58f
zby0902 <zbyroger@gmail.com>
2018-04-29 21:42:24 +1000

    started t2 5196

--- FIT5148_Distributed_Data/assignment2/as2TaskA.js
+++ FIT5148_Distributed_Data/assignment2/as2TaskA.js
@@ -55,3 +55,33 @@ db.getCollection('Fire').aggregate([
     $match:{"Confidence":{$gt:80,$lt:100}}}
 
 ])
+
+/*A6*/
+db.getCollection('Fire').aggregate([
+{$lookup:{from:"Climate",
+          localField:"Date",
+          foreignField:"Date",
+          as:"ClimateData"}
+},
+{
+ $replaceRoot: { newRoot: { $mergeObjects: [ { $arrayElemAt: [ "$ClimateData", 0 ] }, "$$ROOT" ] } }
+},
+{ 
+    $project: { ClimateData: 0,"_id":0}
+},
+{
+    $sort:{"Surface Temperature (Celcius)":-1}
+},
+{
+    $limit:10
+}
+
+/*A7*/
+db.getCollection('Fire').aggregate([
+{$group:{_id:"$Date",NumberOfFire:{$sum:1}}},{$project:{"Date":"$_id","NumberOfFire":1,"_id":0}}
+])
+
+/*A8*/
+db.getCollection('Fire').aggregate([
+{$group:{_id:"$Date",AverageSurfaceTemp:{$avg:"$Surface Temperature (Celcius)"}}},{$project:{"Date":"$_id","AverageSurfaceTemp":1,"_id":0}}
+])
